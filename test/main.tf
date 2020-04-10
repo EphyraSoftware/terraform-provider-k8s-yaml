c36a18adbb6b6b1c708bed40f842b571eb4eef73
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-04-10 21:28:13 +0100

    add test file

---
+++ test/main.tf
@@ -0,0 +1,8 @@
+provider "k8s-yaml" {
+  version = "1.0.0"
+}
+
+resource "k8s-yaml" "test" {
+  name = "dashboard"
+  file_url = "https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-rc7/aio/deploy/recommended.yaml"
+}
