658612e5e3b8f326eb44452f5952a4c85f0aeaea
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-04-11 18:18:19 +0100

    follow naming conventions for resources

--- provider.go
+++ provider.go
@@ -7,7 +7,7 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
-			"k8s-yaml": k8sYaml(),
+			"k8s_yaml": k8sYaml(),
 		},
 	}
 }
